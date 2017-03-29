732e47e350a17a193d7615c297d1d5b6bb27c3d3
Quinn Perfetto <thequinnftw@gmail.com>
2017-03-29 16:13:36 -0400

    pushing brute force method against my will

--- Warcraft II/host.sh
+++ Warcraft II/host.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 unamestr=`uname`
 
-ip=`python getip.py`
+# TODO: test that this actually doesn't work, I have a feeling it does
+#ip=`python getip.py`
+
+if [ "$unamestr" == "Darwin" ]; then
+  ip=$(ifconfig | perl -nle 's/inet ([0-9.]+)/print $1/e' | grep -v 127.0.0.1 | head -n 1)
+else
+  ip=$(ip a | perl -nle 's/inet ([0-9.]+)/print $1/e' | grep -v 127.0.0.1 | head -n 1)
+fi
 
 # TODO: change this to the match maker IP
 matchmaker="10.243.59.165:8080"
