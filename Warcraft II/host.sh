5ab435daf97f58a3be0c67b7e941c9f7451913e7
Quinn Perfetto <thequinnftw@gmail.com>
2017-03-29 15:36:12 -0400

    get around computer not having ifconfig

--- Warcraft II/host.sh
+++ Warcraft II/host.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 unamestr=`uname`
 
-ip=$(ifconfig | perl -nle 's/inet (\S+)/print $1/e' | grep -v 127.0.0.1 | head -n 1)
+ip=`python getip.py`
+
 # TODO: change this to the match maker IP
 matchmaker="10.243.59.165:8080"
 curl "$matchmaker/host?ip=$ip&g=$1"
