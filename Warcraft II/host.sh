254aff7b703de6de635d1c3a6d7a8c8a6cb0d6ea
Quinn Perfetto <thequinnftw@gmail.com>
2017-03-30 15:15:12 -0400

    removed that bruteforce regex

--- Warcraft II/host.sh
+++ Warcraft II/host.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 unamestr=`uname`
 
-# TODO: test that this actually doesn't work, I have a feeling it does
-#ip=`python getip.py`
-
-if [ "$unamestr" == "Darwin" ]; then
-  ip=$(ifconfig | perl -nle 's/inet ([0-9.]+)/print $1/e' | grep -v 127.0.0.1 | head -n 1)
-else
-  ip=$(ip a | perl -nle 's/inet ([0-9.]+)/print $1/e' | grep -v 127.0.0.1 | head -n 1)
-fi
+ip=`python getip.py`
 
 # TODO: change this to the match maker IP
 matchmaker="10.243.59.165:8080"
