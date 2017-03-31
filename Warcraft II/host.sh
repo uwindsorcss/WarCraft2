c481fa7f66f7f1e328c67a1d2c5073c81ad39747
Quinn Perfetto <thequinnftw@gmail.com>
2017-03-31 15:45:25 -0400

    wat

--- Warcraft II/host.sh
+++ Warcraft II/host.sh
@@ -11,7 +11,7 @@ curl "$matchmaker/host?ip=$ip&g=$1"
 tmpfile=$(mktemp ~/dosbox-wc2.XXXXXX)
 cp doshost.conf $tmpfile
 sed -ie "s/%PORT%/1337/g" $tmpfile
-if [ "$unamestr"=="Darwin" ]; then
+if [ "$unamestr"=="Darwin" ]: then
         dosbox -conf $tmpfile
 else
         /usr/bin/ltsp-localapps /usr/bin/dosbox -conf $tmpfile
